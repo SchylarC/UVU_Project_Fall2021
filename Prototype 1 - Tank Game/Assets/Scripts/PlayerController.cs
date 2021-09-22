46bc7aa4ef0ec387ab489a2af650b80f296c71a8
Schylar Christensen <10973475@uvu.edu>
2021-09-22 12:14:23 -0600

    finished tank sim prototype

--- Prototype 1 - Tank Game/Assets/Scripts/PlayerController.cs
+++ Prototype 1 - Tank Game/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-public float speed = 10.5f;
-public float turnSpeed;
+  public float speed;
+  public float turnSpeed;
+
+  public float hInput;
+  public float vInput;
 
 
     // Start is called before the first frame update
@@ -17,6 +20,11 @@ public float turnSpeed;
     // Update is called once per frame
     void Update()
     {
-    transform.Translate(Vector3.forward * speed * Time.deltaTime);  
+      hInput = Input.GetAxis("Horizontal");
+      vInput = Input.GetAxis("Vertical");
+     // Move the tank left and right
+      transform.Rotate(Vector3.up, turnSpeed * hInput * Time.deltaTime);
+    // Moves the tank forward and back 
+      transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput);  
     }
 }
