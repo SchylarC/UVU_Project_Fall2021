2070e82d8594e92ed17341c6542ca61e5400122f
Schylar Christensen <10973475@uvu.edu>
2021-11-05 15:33:47 -0600

    Created Movement controls

--- Prototype 3 - First Person Game/Assets/Scripts/PlayerController.cs
+++ Prototype 3 - First Person Game/Assets/Scripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         // get components
-        camera = camera.main;
+        camera = Camera.main;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -28,24 +28,47 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
+        CamLook();
+
+     
     }
+
+    void FixedUpdate()
+    {
+           if(Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
+
 
     void Move()
     {
         float x = Input.GetAxis("Horizontal") * moveSpeed;
         float z = Input.GetAxis("Vertical") * moveSpeed;
 
-        rb.velocity = new Vector3(x, rb.velocity.y, z);
+        // rb.velocity = new Vector3(x, rb.velocity.y, z); - Old Code
+
+        Vector3 dir = transform.right * x + transform.forward * z;
+        rb.velocity = dir;
+
+
     }
 
-
-    void CamLook()
+     void CamLook()
     {
         float y = Input.GetAxis("Mouse X") * lookSensitivity;
         rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
 
-        
-         
-         
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
+        camera.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;    
+    }
+
+    void Jump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if(Physics.Raycast(ray, 1.1f))
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);         
     }
 }
