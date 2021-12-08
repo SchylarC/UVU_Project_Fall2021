36a2b1615ada376d4fab011be0d23b2b5449e090
Schylar Christensen <10973475@uvu.edu>
2021-12-08 03:36:30 -0700

    Created Pickup scripts and animations

--- Prototype 3 - First Person Game/Assets/Scripts/PlayerController.cs
+++ Prototype 3 - First Person Game/Assets/Scripts/PlayerController.cs
@@ -81,6 +81,17 @@ public class PlayerController : MonoBehaviour
         }           
    
     }
+
+    public void GiveHealth( int amountToGive)
+    {
+        curHp =  Mathf.Clamp(curHp + amountToGive, 0, maxHp);
+    }
+
+    public void GiveAmmo(int amountToGive)
+    {
+        weapon.curAmmo = Mathf.Clamp(weapon.curAmmo + amountToGive, 0, weapon.maxAmmo);
+    }
+
     // Update is called once per frame
     void Update()
     {
