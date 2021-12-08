4a9fb04376bc00fbc958038482d71c56ca74a13d
Schylar Christensen <10973475@uvu.edu>
2021-12-07 23:32:36 -0700

    Created Object Pool

---
+++ Prototype 2 - Top Down/Personal Project Block out/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public float turnspeed = 50.0f;
+    public float hInput;
+    public float vInput;
+
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
