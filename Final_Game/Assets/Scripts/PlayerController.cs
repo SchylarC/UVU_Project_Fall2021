758ec66654820346bd1be61f036088b311f6fd9c
Schylar Christensen <10973475@uvu.edu>
2021-12-10 16:25:43 -0700

    Created Player Controller for final project

---
+++ Final_Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float turnSpeed = 50.0f;
+    public float hInput;
+    public float vInput;
+
+    public float xRange = 14.61f;
+    public float yRange = 7.06f;
+    public Vector3 offset = new Vector3(0,1,0);
+    
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.back, turnSpeed * hInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * vInput * Time.deltaTime);
+
+        // Create walls on the x and -x sides 
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z); 
+        }
+
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3 (xRange, transform.position.y, transform.position.z);
+        }
+
+        // Create walls on the y and -y sides 
+        if(transform.position.y < -yRange)
+        {
+            transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+        }
+
+        if(transform.position.y > yRange)
+        {
+            transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+        }
+    }
+}
--- Prototype 2 - Top Down/Protoype 2 - Top Down/Assets/Scripts/PlayerController.cs
+++ Prototype 2 - Top Down/Protoype 2 - Top Down/Assets/Scripts/PlayerController.cs
@@ -17,8 +17,6 @@ public Transform launcher;
 public Vector3 offset = new Vector3(0,1,0);
     // public float health
 
-    // Start is called before the first frame update
-
     // Update is called once per frame
     void Update()
     {
